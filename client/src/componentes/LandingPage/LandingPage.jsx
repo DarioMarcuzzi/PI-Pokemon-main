f2db8b0c545c1abccefb2671e9cfe9f66f3c17c3
DarioMarcuzzi <darik95@hotmail.com>
2023-01-18 10:52:09 -0300

    landing responsive

--- client/src/componentes/LandingPage/LandingPage.jsx
+++ client/src/componentes/LandingPage/LandingPage.jsx
@@ -1,36 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './landingPage.css'
+import "./landingPage.css";
 
-
-const LandingPage = () =>{
-
+const LandingPage = () => {
   return (
-    <div className="contenedor-Landing-Page">
-      <img className="logo"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png" alt="Pokemon"/>
+    <div className="contenedor-landingPage">
+      <div className="contenedor-logo-landingPage">
+        <img
+          className="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png"
+          alt="Pokemon"
+        />
+      </div>
+      <div className="contenedor-pokebolas-legend">
+        <div className="legend-landingPage">
+          <h1>Pick the pokeball for go Home!</h1>
+        </div>
+        <div className="pokebolas-landingPage">
+          <div className="contenedor-pokemon">
+            <img
+              className="pokemon"
+              id="bulba"
+              src="https://img.pokemondb.net/sprites/black-white/normal/bulbasaur.png"
+              alt="img"
+            />
+            <img
+              className="pokemon"
+              id="char"
+              src="https://img.pokemondb.net/sprites/black-white/normal/charmander.png"
+              alt="img"
+            />
+            <img
+              className="pokemon"
+              id="squir"
+              src="https://img.pokemondb.net/sprites/black-white/normal/squirtle.png"
+              alt="img"
+            />
+          </div>
+          <div className="contenedor-pokebola">
+            <Link to="/pokemons">
+              <img
+                className="pokebola"
+                id="p1"
+                src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png"
+                alt="img"
+              />
+            </Link>
 
-      <div id="pokebolas" className="pokebolas">
-      <link href="http://fonts.cdnfonts.com/css/pokemon-solid" rel="stylesheet"/>
-                
-        <h1>Pick the pokeball for go Home!</h1>
-        <img className="pokemon" id="bulba"  src="https://img.pokemondb.net/sprites/black-white/normal/bulbasaur.png" alt="img"/>
-        <img className="pokemon" id="char" src="https://img.pokemondb.net/sprites/black-white/normal/charmander.png" alt="img"/>
-        <img className="pokemon" id="squir" src="https://img.pokemondb.net/sprites/black-white/normal/squirtle.png" alt="img"/>
-        <div className="contenedor-pokebola">
-        
-        <Link to= '/pokemons'>
-        <img className="pokebola"  id="p1" src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png" alt="img"/></Link>
+            <Link to="/pokemons">
+              <img
+                className="pokebola"
+                id="p2"
+                src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png"
+                alt="img"
+              />
+            </Link>
 
-        <Link to= '/pokemons'>
-        <img className="pokebola"  id="p2"  src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png" alt="img"/></Link>
-        
-        <Link to= '/pokemons'>
-        <img className="pokebola"  id="p3"  src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png" alt="img"/></Link>
+            <Link to="/pokemons">
+              <img
+                className="pokebola"
+                id="p3"
+                src="https://cdn2.bulbagarden.net/upload/thumb/d/dc/GO_Pok%C3%A9_Ball.png/200px-GO_Pok%C3%A9_Ball.png"
+                alt="img"
+              />
+            </Link>
+          </div>
         </div>
       </div>
-      <h1 className="title">PI Henry by <a href="https://www.linkedin.com/in/dario-marcuzzi-399908224" target="_blanck">Dario Marcuzzi</a> </h1>
-    </div> 
-  ) 
-}
+      <h1 className="title">
+        PI Henry by{" "}
+        <a
+          href="https://www.linkedin.com/in/dario-marcuzzi-399908224"
+          target="_blanck"
+        >
+          Dario Marcuzzi
+        </a>{" "}
+      </h1>
+    </div>
+  );
+};
 
 export default LandingPage;
